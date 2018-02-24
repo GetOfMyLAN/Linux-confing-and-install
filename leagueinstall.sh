6da9262e10d5d0692e0aac47c2bdc63c3800777d
Jack <jack@arch.gateway>
2018-02-24 10:41:41 +1000

    fixed league installer

--- leagueinstall.sh
+++ leagueinstall.sh
@@ -1,5 +1,6 @@
-
+wget https://riotgamespatcher-a.akamaihd.net/ShellInstaller/OC1/LeagueofLegends_OC1_Installer_2016_05_13.exe
 WINEPREFIX=~/.LoL64 winecfg
-WINEPREFIX=~/.LoL64 ./winetricks d3dx9
+WINEPREFIX=~/.LoL64 winetricks d3dx9 vcrun2015
+WINEPREFIX=~/.LoL64 winecfg
 WINEPREFIX=~/.LoL64 wine LeagueofLegends_OC1_Installer_2016_05_13.exe
-Add wallpaper:
+
