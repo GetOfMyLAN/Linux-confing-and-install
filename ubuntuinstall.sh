d5149cc46972e9d49a5c4d75882b895624012865
Jack <jack@arch.gateway>
2018-02-24 18:08:28 +1000

    very basic ubuntu install

---
+++ ubuntuinstall.sh
@@ -0,0 +1,3 @@
+sudo apt-get isntall musescore wine winetricks desmume dosbox vbam-wx dolphin-emu mupen64plus zsnes neomutt python-pip krita kdenlive youtube-dl mupdf rofi calcurse compton w3m ffmpeg mpc youtube-viewer noto-fonts-cjk noto-fonts-emoji cmatrix asciiquarium cowsay
+sudo pip install pywal wal-steam
+
