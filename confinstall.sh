b57b08a792272d030d120e7d47e17b6c788bfd96
GitOffMyLAN <jackpsthefirst@gmail.com>
2018-03-23 23:12:52 +1000

    Fixed polybar icons

--- confinstall.sh
+++ confinstall.sh
@@ -1,10 +1,7 @@
-mkdir ~/.config/i3
+cp vim/* ~/
+cp Xresources/* ~/
 cp i3/* ~/.config/i3/*
-mkdir ~/.config/ranger
-cp ranger/* ~/.config/ranger/
-mkdir ~/.mpd
+cp ranger/* ~/.config/ranger
 cp mpd/* ~/.mpd/
-mkdir ~/.ncmpcpp
 cp ncmpcpp/* ~/.ncmpcpp/
-mkdir ~/.config/polybar
 cp polybar/* ~/.config/polybar/
