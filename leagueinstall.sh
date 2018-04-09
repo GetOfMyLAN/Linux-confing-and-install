dbbfe35a2f733752bf65b2e83260831ff6df14c5
GitOffMyLAN <jackpsthefirst@gmail.com>
2018-04-09 17:35:47 +1000

    Delete leagueinstall.sh

--- leagueinstall.sh
+++
@@ -1,6 +0,0 @@
-wget https://riotgamespatcher-a.akamaihd.net/Installer/SingleFileInstall/LeagueOfLegendsBaseEUW.exe
-WINEPREFIX=~/.LoL64 winecfg
-WINEPREFIX=~/.LoL64 winetricks d3dx9 vcrun2015
-WINEPREFIX=~/.LoL64 winecfg
-WINEPREFIX=~/.LoL64 wine LeagueOfLegendsBaseEUW.exe
-
